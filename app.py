508f2826194c55a29820e7f085f6e20df22d304b
Jellyshen <gdshen95@gmail.com>
2016-10-28 13:51:16 +0800

    very basic login function

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, redirect
-from flask_login import LoginManager, login_user, logout_user
+from flask import Flask, redirect, request, flash, render_template
+from flask_login import LoginManager, login_user, logout_user, login_required
+
+from forms import SimpleForm
 from user import User
 from flask_mongoengine import MongoEngine, MongoEngineSessionInterface
 from flask_bcrypt import Bcrypt
@@ -20,13 +22,25 @@ toolbar = DebugToolbarExtension(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+@login_required
+def home():
     return 'hello, world'
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    form = SimpleForm()
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user_obj = User()
+        user_obj.get_by_id('5812dfabe8723d1d64bf3cfa')
+        # if email == user_obj.email and password == user_obj.password:
+        login_user(user_obj)
+        flash("Logged in!")
+        return redirect('/')
+    return render_template('login.html', form=form)
     # user = User()
     # user.email = 'gdshen95@gmail.com'
 
@@ -43,7 +57,7 @@ def load_user(user_id):
         redirect('/login')
     user = User()
     user.get_by_id(user_id)
-    if user.is_active():
+    if user.is_active:
         return user
     else:
         return None
--- forms.py
+++ forms.py
@@ -1,6 +1,8 @@
 import models
+from flask_wtf import Form
+from wtforms import PasswordField
+from wtforms import StringField, PasswordField
 from flask_mongoengine.wtf import model_form
-from wtforms.fields import *
 from flask_mongoengine.wtf.orm import validators
 
 user_form = model_form(models.User, exclude=['password'])
@@ -8,9 +10,15 @@ user_form = model_form(models.User, exclude=['password'])
 
 # Signup Form created from user_form
 class SignupForm(user_form):
-    password = PasswordField('Password', validators=[validators.DataRequired(), validators.EqualTo('confirm', message='Passwords must match')])
+    password = PasswordField('Password', validators=[validators.DataRequired(),
+                                                     validators.EqualTo('confirm', message='Passwords must match')])
     confirm = PasswordField('Repeat Password')
 
 
 class LoginForm(user_form):
-        password = PasswordField('Password', validators=[validators.DataRequired()])
+    password = PasswordField('Password', validators=[validators.DataRequired()])
+
+
+class SimpleForm(Form):
+    email = StringField()
+    password = PasswordField()
