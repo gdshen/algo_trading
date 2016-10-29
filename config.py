1a6e6fcb98f7868ec84ae7ec4b1e7dd28bce5b23
gdshen <gdshen95@gmail.com>
2016-10-29 17:52:31 +0800

    add ssl_or_not

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ SECRET_KEY = 'this_is_a_secret_key'
 DEBUG_TB_PANELS = ['flask_mongoengine.panels.MongoDebugPanel']
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 
+SSL_OR_NOT = True
 MONGODB_SETTINGS = {
     'db': 'trade',
     'host': '127.0.0.1',
--- run_server.py
+++ run_server.py
@@ -1,5 +1,8 @@
 from app import app
 
 if __name__ == '__main__':
-    context = ('trade_gdshen_me.crt', 'trade_gdshen_me.key')
-    app.run(port=443, ssl_context=context)
+    if app.config['SSL_OR_NOT']:
+        context = ('trade_gdshen_me.crt', 'trade_gdshen_me.key')
+        app.run(port=443, ssl_context=context)
+    else:
+        app.run(port=80)
