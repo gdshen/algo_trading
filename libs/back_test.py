a35cc0ce178be41beea8e2eab5a9776482e262b2
CJ528 <528122401@qq.com>
2016-12-24 13:08:53 +0800

    back test: 分段vwap比较，用dataframe存储

--- libs/back_test.py
+++ libs/back_test.py
@@ -1,8 +1,9 @@
 import libs.data_persist_mongodb as it
+import pandas as pd
 import datetime
 
 def backtest():
-    result = dict()
+    #result = list()
 
     __predicted_vwap = dict()
     __predicted_vwap['stock'] = "600000"
@@ -29,20 +30,33 @@ def backtest():
     #                                 predicted_vwap['afternoon_start'], predicted_vwap['afternoon_stop'])
 
 
-    actual_vwap = sum(data_of_today['amount']) / sum(data_of_today['volume']) / 100
+    # actual_vwap = sum(data_of_today['amount']) / sum(data_of_today['volume']) / 100
+    # __policy = __predicted_vwap['policy']
+    #
+    # datetime_format = '%Y-%m-%d%H:%M:%S'
+    # amount = 0.0
+    # volume = 0
+    # for i in __policy:
+    #     volume += i[1]
+    #     start = datetime.datetime.strptime(__predicted_vwap['day'] + i[0], datetime_format)
+    #     end = start + datetime.timedelta(minutes=1)
+    #     amount += i[1] * data_of_today['price'][(data_of_today['time'] > start) & (data_of_today['time'] < end)].iloc[0]
+    #
+    # predicted_vwap = amount / volume
+    # result['actual_vwap'] = actual_vwap
+    # result['predicted_vwap'] = predicted_vwap
+
+    columns = ['actual_vwap','predicted_vwap']
+    result = pd.DataFrame(columns=columns)
     __policy = __predicted_vwap['policy']
-
     datetime_format = '%Y-%m-%d%H:%M:%S'
-    amount = 0.0
-    volume = 0
-    for i in __policy:
-        volume += i[1]
-        start = datetime.datetime.strptime(__predicted_vwap['day'] + i[0], datetime_format)
-        end = start + datetime.timedelta(minutes=1)
-        amount += i[1] * data_of_today['price'][(data_of_today['time'] > start) & (data_of_today['time'] < end)].iloc[0]
+    for index in range(len(__policy) - 1):
+        start = datetime.datetime.strptime(__predicted_vwap['day'] + __policy[index][0], datetime_format)
+        end = datetime.datetime.strptime(__predicted_vwap['day'] + __policy[index + 1][0], datetime_format)
 
-    predicted_vwap = amount / volume
-    result['actual_vwap'] = actual_vwap
-    result['predicted_vwap'] = predicted_vwap
+        actual_vwap_data = data_of_today[['amount', 'volume']][(data_of_today['time'] > start) & (data_of_today['time'] < end)]
+        actual_vwap = sum(actual_vwap_data['amount']) / sum(actual_vwap_data['volume']) / 100
+        predicted_vwap = data_of_today['price'][(data_of_today['time'] > start) & (data_of_today['time'] < start + datetime.timedelta(minutes=1))].iloc[0]
 
+        result.loc[index] = [actual_vwap, predicted_vwap]
     return result
