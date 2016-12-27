35507d75b2936924adddd520bbba5af8ce238954
Guodong Shen <gdshen95@gmail.com>
2016-12-27 17:08:07 +0800

    rewrite home page

--- app.py
+++ app.py
@@ -31,28 +31,28 @@ flask_bcrypt = Bcrypt(app)
 # toolbar = DebugToolbarExtension(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
-@login_required
-def home():
-    form = OperationForm(request.form)
-    rows = []
-    if request.method == 'POST':
-        flash("Slicing orders on server")
-        logging.debug(form.security.data)
-        logging.debug(form.shares.data)
-        logging.debug(form.operation.data)
-        logging.debug(form.methods.data)
-
-        user_id = str(current_user.get_id())
-        stock = form.security.data
-        action = int(form.operation.data)
-        volume = int(form.shares.data)
-        method = form.methods.data
-
-        algo_trade_engine = zerorpc.Client('tcp://127.0.0.1:4242')
-        rows = algo_trade_engine.slicing_order(user_id, stock, action, volume, method)
-
-    return render_template('home.html', email=current_user.email, form=form, rows=rows)
+# @app.route('/', methods=['GET', 'POST'])
+# @login_required
+# def home():
+#     form = OperationForm(request.form)
+#     rows = []
+#     if request.method == 'POST':
+#         flash("Slicing orders on server")
+#         logging.debug(form.security.data)
+#         logging.debug(form.shares.data)
+#         logging.debug(form.operation.data)
+#         logging.debug(form.methods.data)
+#
+#         user_id = str(current_user.get_id())
+#         stock = form.security.data
+#         action = int(form.operation.data)
+#         volume = int(form.shares.data)
+#         method = form.methods.data
+#
+#         algo_trade_engine = zerorpc.Client('tcp://127.0.0.1:4242')
+#         rows = algo_trade_engine.slicing_order(user_id, stock, action, volume, method)
+#
+#     return render_template('home.html', email=current_user.email, form=form, rows=rows)
 
 
 @app.route('/test', methods=['GET'])
@@ -73,14 +73,14 @@ def hello():
         return "NoData"
 
 
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 @login_required
-def user_home():
+def home():
     if request.method == 'GET':
-        return render_template('user_home.html')
+        return render_template('home.html')
     elif request.method == 'POST':
         data = request.get_json()
-        # print(data)
+        print(data)
         return 'success'
 
 
