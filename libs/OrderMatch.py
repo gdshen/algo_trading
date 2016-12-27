eda1e8c58034a5c895d25a8c16c497d6a02a3061
CJ528 <528122401@qq.com>
2016-12-28 00:15:56 +0800

    order_match

--- libs/OrderMatch.py
+++ libs/OrderMatch.py
@@ -12,13 +12,6 @@ from libs.data_persist_redis import retrieve_from_redis
 from predict.predictAPI import getDayVolume
 
 
-# class OrderMatch:
-#     # def __init__(self):
-#     #     self.policy = self.retrieve_policy_from_db()
-#     #
-#     # def retrieve_policy_from_db(self):
-
-
 def read_from_redis():
     r = redis.StrictRedis(REDIS_SERVER_HOST, REDIS_SERVER_PORT, REDIS_SERVER_DB)
     keys = r.keys()
@@ -32,25 +25,25 @@ def read_from_redis():
 
 
 def order_match():
-    users = read_from_redis()
     while True:
+        users = read_from_redis()
         for user in users:
             user_policys = users[user]
             for user_policy in user_policys:
                 for user_policy_element in user_policy['policy']:
                     order_point = user_policy_element[1][0]
                     order_num = user_policy_element[1][1]
-                    if datetime.datetime.now().strftime("%X") == order_point:
-                        current_data = retrieve_from_redis(user_policy['stock']).iloc[-1]
+                    if (datetime.datetime.now()+datetime.timedelta(minutes=550)).strftime("%X") == order_point:
+                        current_data = retrieve_from_redis(user_policy['stock'])
                         if user_policy['wap'] == 'twap' or user_policy['wap'] == 'vwap':
                             if user_policy['order_type'] == 'buy':
-                                if current_data['a1_v'] < order_num:
+                                if current_data['a1_v'].iloc[-1] < order_num:
                                     logging.warning(str(user) + "在" + str(order_point) + "时刻要买入的关于" + user_policy[
                                         'stock'] + "股票的一单失败")
                                 else:
                                     pass
                             else:
-                                if current_data['b1_v'] < order_num:
+                                if current_data['b1_v'].iloc[-1] < order_num:
                                     logging.warning(str(user) + "在" + str(order_point) + "时刻要卖出的关于" + user_policy[
                                         'stock'] + "股票的一单失败")
                         elif user_policy['wap'] == 'vwap_with_predict':
@@ -60,11 +53,11 @@ def order_match():
                             start_2min = start_1min + datetime.timedelta(minutes=-1)
                             start_3min = start_2min + datetime.timedelta(minutes=-1)
 
-                            price_3min = current_data['price', 'volume'][
+                            price_3min = current_data[['price', 'volume']][
                                 (current_data['time'] >= start_3min) & (current_data['time'] <= start_2min)]
-                            price_2min = current_data['price', 'volume'][
+                            price_2min = current_data[['price', 'volume']][
                                 (current_data['time'] >= start_2min) & (current_data['time'] <= start_1min)]
-                            price_1min = current_data['price', 'volume'][
+                            price_1min = current_data[['price', 'volume']][
                                 (current_data['time'] >= start_1min) & (current_data['time'] <= end_1min)]
 
                             input_list = [price_1min['price'].iloc[0], price_1min['price'].iloc[-1],
@@ -80,52 +73,75 @@ def order_match():
                             next_min_trend = getDayVolume(input_list, user_policy['stock'])
                             if user_policy['order_type'] == 'buy':
                                 if next_min_trend == 1:
-                                    random_num = random.random()
-                                    if random_num > 0.1:
-                                        if current_data['a1_v'] < order_num:
-                                            logging.warning(
-                                                str(user) + "在" + str(order_point) + "时刻要买入的关于" + user_policy[
-                                                    'stock'] + "股票的一单失败")
-                                        else:
-                                            pass
+                                    threshold_value = 0.1
+                                else:
+                                    threshold_value = 0.4
+                                if random.random() > threshold_value:
+                                    if current_data['a1_v'].iloc[-1] < order_num:
+                                        logging.warning(
+                                            str(user) + "在" + str(order_point) + "时刻要买入的关于" + user_policy[
+                                                'stock'] + "股票的一单失败")
                                     else:
-                                        user_policy_element[1][0] = (end_1min + datetime.timedelta(minutes=1)).strftime("%X")
+                                        pass
+                                else:
+                                    if ((end_1min + datetime.timedelta(minutes=1)).strftime("%X") <=
+                                            user_policy_element[0][1]):              ### 判断加一分钟是否超出时间段
+                                        user_policy_element[1][0] = (end_1min + datetime.timedelta(minutes=1)).strftime(
+                                            "%X")
                                         r = redis.StrictRedis(REDIS_SERVER_HOST, REDIS_SERVER_PORT, REDIS_SERVER_DB)
                                         r.delete(user)
                                         for u in users[user]:
                                             r.rpush(user, json.dumps(u))
+                                    else:
+                                        pass
+                            elif user_policy['order_type'] == 'sell':
+                                if next_min_trend == 1:
+                                    threshold_value = 0.8
                                 else:
-                                    pass
-
-
-
-
+                                    threshold_value = 0.6
+                                if random.random() > threshold_value:
+                                    if current_data['b1_v'].iloc[-1] < order_num:
+                                        logging.warning(str(user) + "在" + str(order_point) + "时刻要卖出的关于" + user_policy[
+                                            'stock'] + "股票的一单失败")
+                                    else:
+                                        pass
+                                else:
+                                    if ((end_1min + datetime.timedelta(minutes=1)).strftime("%X") <=
+                                            user_policy_element[0][1]):
+                                        user_policy_element[1][0] = (end_1min + datetime.timedelta(minutes=1)).strftime(
+                                            "%X")
+                                        r = redis.StrictRedis(REDIS_SERVER_HOST, REDIS_SERVER_PORT, REDIS_SERVER_DB)
+                                        r.delete(user)
+                                        for u in users[user]:
+                                            r.rpush(user, json.dumps(u))
+                                    else:
+                                        pass
                     else:
                         continue
         time.sleep(1)
 
 
 if __name__ == '__main__':
-    users = read_from_redis()
-    for user in users:
-        user_policys = users[user]
-        for user_policy in user_policys:
-            #pprint(user_policy['policy'])
-            for i in user_policy['policy']:
-                order_point = i[1][0]
-                if i[1][1] == 194:
-                    i[1][1] = 10970
-                r = redis.StrictRedis(REDIS_SERVER_HOST, REDIS_SERVER_PORT, REDIS_SERVER_DB)
-                r.delete(user)
-                for u in users[user]:
-                    r.rpush(user, json.dumps(u))
-                # b = [1,2,3,4,5]
-                # a = datetime(2016,12,23,21,1,35)
-                # for i in b:
-                #     while 1:
-                #         if a.strftime("%X") == datetime.now().strftime("%X"):
-                #             print(i)
-                #             break
-                #         else:
-                #             time.sleep(1)
-                #     time.sleep(1)
+    order_match()
+    # users = read_from_redis()
+    # pprint(users)
+    # for user in users:
+    #     user_policys = users[user]
+    #     pprint(users[user])
+        # for user_policy in user_policys:
+        #     pprint(user_policy)
+            # for i in user_policy['policy']:
+            #     order_point = i[1][0]
+            #     if i[1][1] == 194:
+            #         i[1][1] = 10970
+            #     r = redis.StrictRedis(REDIS_SERVER_HOST, REDIS_SERVER_PORT, REDIS_SERVER_DB)
+            #     r.delete(user)
+            #     for u in users[user]:
+            #         r.rpush(user, json.dumps(u))
+            # a = ['23:25:00', '23:25:15', '23:25:30']
+            # while True:
+            #     for i in a:
+            #         if datetime.datetime.now().strftime("%X") == i:
+            #             print("******")
+            #     time.sleep(1)
+
--- libs/policy/VWAP.py
+++ libs/policy/VWAP.py
@@ -5,11 +5,10 @@ from predict.predictAPI import getDayVolume
 
 
 class VWAP(NDayMean):
-    def __init__(self, stock, date, order_type, tmr_trend, n_days=7, n_slice=10):
+    def __init__(self, stock, date, n_days=7, n_slice=10):
         super().__init__(stock, date, n_days, n_slice)
         self.strategy = "vwap_with_predict"
-        self.order_type = order_type
-        self.trend = tmr_trend
+
 
     def wap(self, order_amount, time_intervals):
         time_list = self.time_slice(time_intervals, self.n_slice)
@@ -31,9 +30,9 @@ class VWAP(NDayMean):
 
 
 if __name__ == '__main__':
-    seven_day_mean = VWAP('601398', '2016-12-21', 'buy', 'up')
-    l = seven_day_mean.time_slice([('09:30:00', '10:30:00'), ('13:00:00', "13:40:00")], 8)
+    seven_day_mean = VWAP('601398', '2016-12-23')
+    l = seven_day_mean.time_slice([('09:30:00', '09:45:00')], 8)
     # pprint(l)
-    l = seven_day_mean.wap(1000, [('09:30:00', '10:30:00'), ('13:00:00', "13:40:00")])
+    l = seven_day_mean.wap(1000, [('09:30:00', '09:45:00')])
     pprint(l)
-    print(seven_day_mean.save(1000, [('09:30:00', '10:30:00'), ('13:00:00', "13:40:00")]))
+    print(seven_day_mean.save('123456', 'buy', 1000, [('09:30:00', '09:45:00')]))
